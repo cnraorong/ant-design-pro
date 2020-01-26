91d8505bdea19e35780bf853eded08a224f22bb3
sorrycc <sorrycc@gmail.com>
2020-01-26 10:28:51 +0800

    fix config

--- config/config.ts
+++ config/config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'umi';
+import { defineConfig, utils } from 'umi';
 
 // preview.pro.ant.design only do not use in your production ;
 // preview.pro.ant.design 专用环境变量，请不要在你的项目中使用它。
@@ -6,13 +6,10 @@ const { ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION } = process.env;
 
 export default defineConfig({
   plugins: ['@umijs/plugin-dva', '@umijs/plugin-antd', '@umijs/plugin-locale'],
-  antd: true,
   dva: {
     hmr: true,
   },
   locale: {
-    // default false
-    enable: true,
     // default zh-CN
     default: 'zh-CN',
     // default true, when it is true, will use `navigator.language` overwrite default
@@ -83,37 +80,37 @@ export default defineConfig({
   lessLoader: {
     javascriptEnabled: true,
   },
-  cssLoader: {
-    modules: true,
-    getLocalIdent: (
-      context: {
-        resourcePath: string;
-      },
-      _: string,
-      localName: string,
-    ) => {
-      if (
-        context.resourcePath.includes('node_modules') ||
-        context.resourcePath.includes('ant.design.pro.less') ||
-        context.resourcePath.includes('global.less')
-      ) {
-        return localName;
-      }
-
-      const match = context.resourcePath.match(/src(.*)/);
-
-      if (match && match[1]) {
-        const antdProPath = match[1].replace('.less', '');
-        const arr = slash(antdProPath)
-          .split('/')
-          .map((a: string) => a.replace(/([A-Z])/g, '-$1'))
-          .map((a: string) => a.toLowerCase());
-        return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
-      }
-
-      return localName;
-    },
-  },
+  // cssLoader: {
+  //   modules: true,
+  //   getLocalIdent: (
+  //     context: {
+  //       resourcePath: string;
+  //     },
+  //     _: string,
+  //     localName: string,
+  //   ) => {
+  //     if (
+  //       context.resourcePath.includes('node_modules') ||
+  //       context.resourcePath.includes('ant.design.pro.less') ||
+  //       context.resourcePath.includes('global.less')
+  //     ) {
+  //       return localName;
+  //     }
+  //
+  //     const match = context.resourcePath.match(/src(.*)/);
+  //
+  //     if (match && match[1]) {
+  //       const antdProPath = match[1].replace('.less', '');
+  //       const arr = utils.winPath(antdProPath)
+  //         .split('/')
+  //         .map((a: string) => a.replace(/([A-Z])/g, '-$1'))
+  //         .map((a: string) => a.toLowerCase());
+  //       return `antd-pro${arr.join('-')}-${localName}`.replace(/--/g, '-');
+  //     }
+  //
+  //     return localName;
+  //   },
+  // },
   // chainWebpack: webpackPlugin,
   // proxy: {
   //   '/server/api/': {
